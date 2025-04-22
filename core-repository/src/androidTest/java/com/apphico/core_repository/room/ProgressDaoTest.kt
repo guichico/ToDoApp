41a1e0b42fd55894096d7fa2d5d16e604dcf5a3b
Guilherme Chiconato <guilherme.chiconato@ndrive.com>
2025-04-22 16:46:15 -0300

    ProgressDaoTest

--- core-repository/src/androidTest/java/com/apphico/core_repository/room/ProgressDaoTest.kt
+++ core-repository/src/androidTest/java/com/apphico/core_repository/room/ProgressDaoTest.kt
@@ -4,10 +4,21 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.apphico.core_model.Achievement
+import com.apphico.core_model.MeasurementType
 import com.apphico.core_repository.calendar.room.AppDatabase
+import com.apphico.core_repository.calendar.room.dao.AchievementDao
 import com.apphico.core_repository.calendar.room.dao.ProgressDao
+import com.apphico.core_repository.calendar.room.entities.ProgressDB
+import com.apphico.core_repository.calendar.room.entities.toAchievement
+import com.apphico.core_repository.calendar.room.entities.toAchievementDB
+import com.apphico.extensions.getNowDate
+import com.apphico.extensions.getNowTime
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
@@ -16,6 +27,7 @@ class ProgressDaoTest {
 
     private lateinit var db: AppDatabase
 
+    private lateinit var achievementDao: AchievementDao
     private lateinit var progressDao: ProgressDao
 
     @Before
@@ -23,6 +35,7 @@ class ProgressDaoTest {
         val appContext = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(appContext, AppDatabase::class.java).build()
 
+        achievementDao = db.achievementDao()
         progressDao = db.progressDao()
     }
 
@@ -30,5 +43,29 @@ class ProgressDaoTest {
     @Throws(IOException::class)
     fun closeDb() {
         db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndDelete() {
+        runBlocking {
+            val achievementId = achievementDao.insert(Achievement(name = "Achievement test", measurementType = MeasurementType.Percentage()).toAchievementDB())
+
+            progressDao.insertAll(
+                listOf(
+                    ProgressDB(0, achievementId, 0.3f, "Progress 1", getNowDate(), getNowTime()),
+                    ProgressDB(0, achievementId, 0.6f, "Progress 2", getNowDate(), getNowTime()),
+                    ProgressDB(0, achievementId, 1f, "Progress 3", getNowDate(), getNowTime())
+                )
+            )
+
+            assert(progressDao.getAll().first().size == 3)
+            assert(achievementDao.getAchievement(achievementId).toAchievement().getProgress() == 1f)
+
+            progressDao.deleteAll(achievementId, emptyList())
+
+            assert(progressDao.getAll().first().isEmpty())
+            assert(achievementDao.getAchievement(achievementId).toAchievement().getProgress() == 0f)
+        }
     }
 }
