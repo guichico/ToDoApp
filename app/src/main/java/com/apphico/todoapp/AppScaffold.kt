134600301631a585ecaa3d9cf4609d57bc3b79b3
Guilherme Chiconato <guilherme.chiconato@ndrive.com>
2025-06-15 02:59:11 -0300

    optimized imports

--- app/src/main/java/com/apphico/todoapp/AppScaffold.kt
+++ app/src/main/java/com/apphico/todoapp/AppScaffold.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
